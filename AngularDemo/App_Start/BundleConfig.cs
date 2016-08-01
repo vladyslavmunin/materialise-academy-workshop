b59756ef33edfdd11e27d8bd6f9e606b2dd2426e
tamatsyk <taisecu@gmail.com>
2016-08-01 15:06:53 +0300

    initial commit

---
+++ AngularDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace AngularDemo.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/angular")
+                    .IncludeDirectory("~/Scripts", "angular-*.js")
+                    );
+        }
+    }
+}
