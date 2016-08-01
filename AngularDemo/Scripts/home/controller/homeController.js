759180e6114a6b6f3a27f26a56e363d8efe31d43
tamatsyk <taisecu@gmail.com>
2016-08-01 16:47:34 +0300

    add factory

--- AngularDemo/Scripts/home/controller/homeController.js
+++ AngularDemo/Scripts/home/controller/homeController.js
@@ -5,15 +5,28 @@
         .module('angularDemoApp')
         .controller('homeController', homeController);
 
-    homeController.$inject = ['$location']; 
+    homeController.$inject = ['criminalFactory'];
 
-    function homeController($location) {
+    function homeController(criminalFactory) {
         /* jshint validthis:true */
         var vm = this;
+        vm.getPokemon = getPokemon; 
         vm.title = 'homeController';
         
         activate();
 
-        function activate() { }
+        function activate() {
+            vm.getPokemon();
+        }
+
+        function getPokemon() {
+            criminalFactory.getPokemon()
+                .success(function (result) {
+                    vm.pokemon = result;
+                })
+                .catch(function (exception) {
+                    var ex = exception;
+                });
+        };
     }
 })();
---
+++ AngularDemo/Scripts/home/services/criminalFactory.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('angularDemoApp')
+        .factory('criminalFactory', criminalFactory);
+
+    criminalFactory.$inject = ['$http'];
+
+    function criminalFactory($http) {
+        var service = {
+            getData: getData,
+            getPokemon: getPokemon
+        };
+
+        return service;
+
+        function getData() {
+            alert('hello, i am a new factory');
+        }
+
+        function getPokemon() {
+            return $http.get("https://pokeapi.co/api/v2/pokemon-form/1/");
+        }
+    }
+})();
