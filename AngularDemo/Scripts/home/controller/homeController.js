73cd536445d4b64425a60250490b310e0e616fd0
tamatsyk <taisecu@gmail.com>
2016-08-01 16:01:54 +0300

    Add first controller

---
+++ AngularDemo/Scripts/home/controller/homeController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('angularDemoApp')
+        .controller('homeController', homeController);
+
+    homeController.$inject = ['$location']; 
+
+    function homeController($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'homeController';
+        
+        activate();
+
+        function activate() { }
+    }
+})();
