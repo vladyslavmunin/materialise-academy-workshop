9a0819a6ad7b1ac48d6d17c8325a723a160634e8
Vladyslav Munin <vladcisco.vm@hotmail.com>
2016-08-02 21:21:15 +0300

    add directive

---
+++ AngularDemo/Scripts/home/directives/nameDirective.js
@@ -0,0 +1,37 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('angularDemoApp')
+        .directive('nameDirective', nameDirective);
+
+    nameDirective.$inject = ['$window'];
+    
+    function nameDirective ($window) {
+      
+        var directive = {
+            link: link,
+            require: '?ngModel',
+            restrict: 'A'
+        
+        };
+        return directive;
+
+        function link(scope, element, attrs, mCtrl) {
+            function ValidateName(value) {
+                if (!mCtrl) {
+                    return;
+                }
+                if (value === 'Vlados') {
+                    mCtrl.$setValidity('name',true);
+                }
+                else {
+                    mCtrl.$setValidity('name', false);
+                }
+                return value;
+            }
+            mCtrl.$parsers.push(ValidateName);
+        };
+    }
+
+})();
